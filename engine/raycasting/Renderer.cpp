416d14a841f9b60b54d4a98d89f62039662a2629
Tisca Catalin <admin@tiscacatalin.com>
2025-10-26 18:57:46 +0200

    clearning imports

--- engine/raycasting/Renderer.cpp
+++ engine/raycasting/Renderer.cpp
@@ -1,9 +1,6 @@
 #include "Renderer.h"
 
-#include "../../utils/Logging.h"
-
 namespace engine::raycasting {
     void Renderer::render(Window &window) const {
-        // logging::error("raycasting not implemented yet");
     }
 } // raycasting
